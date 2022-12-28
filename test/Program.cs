536e5a6fdbfb4f37284560fbe9d977826d37131c
Denis_Zeff <d.sujew@gmail.com>
2022-12-28 14:58:22 +0300

    added test code

---
+++ test/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите массив строк в кавычках и через запятую:");
+string output = Console.ReadLine();
+char[] separators = new char[] { ' ', ',' };
+String[] result = output.Split(separators);
+
+
+int count = 0;
+
+        for (int i = 0; i < result.Length; i++) 
+        {
+         if (result[i].Length<=5)
+            {
+                count++;  
+            }
+        }
+
+string[] resume = new string[count];
+int j = 0;
+for ( int i=0; i < result.Length; i++)
+    { 
+        if (result[i].Length<=5)
+            {
+                resume[j]= result[i];
+                j++;
+            }
+    }
+
+WriteArray(resume);
+
+void WriteArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
